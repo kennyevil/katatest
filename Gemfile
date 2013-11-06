7820ac5e6790377e253686d32dba4acb23d676e8
Chris Gannon <cjgannon@gmail.com>
2013-11-06 16:16:58 +0000

    created arrays for n-lettered words where 0 < n <= 6

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'numbers_and_words'
--- readable.rb
+++ readable.rb
@@ -1,13 +1,32 @@
 #!/usr/bin/ruby
 
-dictionary = File.foreach('dictionary.txt').map { |line| line.split(' ') }
+
+dictionary = File.foreach('dictionary.txt').map { |line| line.split('\n') }
 
 one_letter_words = Array.new
+two_letter_words = Array.new
+three_letter_words = Array.new
+four_letter_words = Array.new
+five_letter_words = Array.new
+six_letter_words = Array.new
 
 for word in dictionary do
-  if word.size == 1 
-    one_letter_words.push(word) 
+  if word.to_s.length == 7 
+    one_letter_words.push(word)
+  elsif word.to_s.length == 8
+    two_letter_words.push(word)
+  elsif word.to_s.length == 9
+    three_letter_words.push(word)
+  elsif word.to_s.length == 10
+    four_letter_words.push(word)
+  elsif word.to_s.length == 11
+    five_letter_words.push(word) 
+  elsif word.to_s.length == 12
+    six_letter_words.push(word)
   end
 end
 
-puts one_letter_words
+puts "There are #{one_letter_words.size} one-letter words"
+puts "There are #{two_letter_words.size} two letter words"
+puts "There are #{six_letter_words.size} six letter words"
+
